48f19b45ddf1fe42196d70cee66e06cd64b43c84
YunSwain <destap@naver.com>
2024-11-08 13:08:54 +0900

    feat: 재사용할 TextField 수정자 선언

---
+++ Vocabulary/DesignSystem/HandwritableTextFieldStyle.swift
@@ -0,0 +1,27 @@
+//
+//  TextFieldStyle.swift
+//  Vocabulary
+//
+//  Created by Swain Yun on 11/7/24.
+//
+
+import SwiftUI
+
+struct HandwritableTextFieldStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .font(.title3.bold())
+            .frame(maxWidth: .infinity)
+            .background(.thinMaterial)
+            .clipShape(.rect(cornerRadius: 14))
+            .autocorrectionDisabled()
+            .textInputAutocapitalization(.never)
+    }
+}
+
+extension View {
+    func handwritableTextFieldStyle() -> some View {
+        self.modifier(HandwritableTextFieldStyle())
+    }
+}
